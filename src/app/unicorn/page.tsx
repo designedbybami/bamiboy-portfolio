601304f8b76b4945267e1acc24ffd7fbfaf50844
designedbybami <akinadebolu123@gmail.com>
2026-04-19 17:50:13 +0100

    feat: Testing UnicornHero component and integrate unicornstudio-react

---
+++ src/app/unicorn/page.tsx
@@ -0,0 +1,10 @@
+import UnicornHero from "@/components/shared/unicorn-hero";
+
+export default function UnicornPage() {
+  return (
+    <main>
+      <h1>Testing Unicorn in Next.js</h1>
+      <UnicornHero />
+    </main>
+  );
+}
